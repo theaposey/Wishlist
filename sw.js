43a317aabefa8775b0754e788df2812d2f18d62c
Alathea <theaposey@mac.mynetworksettings.com>
2026-04-17 09:12:00 -0400

    Fix share URL length: restore compression, keep image upload in background

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wishlist-v37';
+const CACHE = 'wishlist-v38';
 const BASE = '/Wishlist';
 const SHELL = [BASE + '/index.html', BASE + '/manifest.json', BASE + '/sw.js', BASE + '/icon.png', BASE + '/install.html'];
 
