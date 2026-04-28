8a54bf29d08e6af6ee6c1ad5b702fdd6df86f105
Alathea <theaposey@mac.mynetworksettings.com>
2026-04-27 23:46:16 -0400

    Boards as wishlist cover option

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wishlist-v41';
+const CACHE = 'wishlist-v42';
 const BASE = '/Wishlist';
 const SHELL = [BASE + '/index.html', BASE + '/manifest.json', BASE + '/sw.js', BASE + '/icon.png', BASE + '/install.html'];
 
