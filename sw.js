cd84062bc391a1f6fce7bb0bae502ec12d2d38d4
Alathea <theaposey@mac.mynetworksettings.com>
2026-05-25 07:59:16 -0400

    Fix list thumb cropping, iOS keyboard Done button, board card ratio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'wishlist-v76';
+const CACHE = 'wishlist-v78';
 const BASE = '/Wishlist';
 const SHELL = [BASE + '/index.html', BASE + '/manifest.json', BASE + '/sw.js', BASE + '/icon.png', BASE + '/install.html'];
 
